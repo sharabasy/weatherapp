0f907ae8d49904fc941422ff019992b24d815f32
Abdelrahman El Sharabasy <boodysharabas@gmail.com>
2025-03-24 05:59:29 +0200

    searchable select inputs

--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from "react";
+import Select from "react-select";
 import { Country } from "country-state-city";
 
 const AppForm = (props) => {
-  const [selectedCountry, setSelectedCountry] = useState("");
-  const [selectedCity, setSelectedCity] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
   const [cities, setCities] = useState([]);
 
   const fetchCities = async (countryCode) => {
     try {
       const response = await fetch("/city.list.json");
       const data = await response.json();
-
-      const filteredCities = data.filter(
-        (city) => city.country === countryCode
-      );
+      const filteredCities = data
+        .filter((city) => city.country === countryCode)
+        .map((city) => ({ value: city.name, label: city.name }));
       setCities(filteredCities);
     } catch (error) {
       console.error("Error loading city list:", error);
@@ -23,8 +23,8 @@ const AppForm = (props) => {
 
   useEffect(() => {
     if (selectedCountry) {
-      setSelectedCity("");
-      fetchCities(selectedCountry);
+      setSelectedCity(null);
+      fetchCities(selectedCountry.value);
     } else {
       setCities([]);
     }
@@ -32,41 +32,74 @@ const AppForm = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.getWeather(selectedCountry, selectedCity);
+    if (selectedCountry && selectedCity) {
+      props.getWeather(selectedCountry.value, selectedCity.value);
+    } else {
+      props.getWeather(null, null);
+    }
   };
+
+  const countryOptions = Country.getAllCountries().map((country) => ({
+    value: country.isoCode,
+    label: country.name,
+  }));
 
   return (
     <form onSubmit={handleSubmit}>
       <h1>Weather App</h1>
 
-      {/* Country Dropdown */}
-      <select
-        name="country"
+      {/* Searchable Country Dropdown */}
+      <Select
+        options={countryOptions}
         value={selectedCountry}
-        onChange={(e) => setSelectedCountry(e.target.value)}
-      >
-        <option value="">Select Country</option>
-        {Country.getAllCountries().map((country) => (
-          <option key={country.isoCode} value={country.isoCode}>
-            {country.name}
-          </option>
-        ))}
-      </select>
+        onChange={setSelectedCountry}
+        placeholder="Select Country"
+        isSearchable
+        className="react-select-container"
+        classNames={{
+          control: () => "react-select__control",
+          placeholder: (state) =>
+            state.isFocused
+              ? "react-select__placeholder react-select__placeholder--is-focused"
+              : "react-select__placeholder",
+          singleValue: () => "react-select__single-value",
+          menu: () => "react-select__menu",
+          input: () => "react-select__input",
+          option: (state) =>
+            state.isFocused
+              ? "react-select__option react-select__option--is-focused"
+              : state.isSelected
+              ? "react-select__option react-select__option--is-selected"
+              : "react-select__option",
+        }}
+      />
 
-      {/* City Dropdown */}
-      <select
-        name="city"
+      {/* Searchable City Dropdown */}
+      <Select
+        options={cities}
         value={selectedCity}
-        onChange={(e) => setSelectedCity(e.target.value)}
-        disabled={!selectedCountry || cities.length === 0}
-      >
-        <option value="">Select City</option>
-        {cities.map((city) => (
-          <option key={city.id} value={city.name}>
-            {city.name}
-          </option>
-        ))}
-      </select>
+        onChange={setSelectedCity}
+        placeholder="Select City"
+        isSearchable
+        isDisabled={!selectedCountry || cities.length === 0}
+        className="react-select-container"
+        classNames={{
+          control: () => "react-select__control",
+          placeholder: (state) =>
+            state.isFocused
+              ? "react-select__placeholder react-select__placeholder--is-focused"
+              : "react-select__placeholder",
+          singleValue: () => "react-select__single-value",
+          menu: () => "react-select__menu",
+          input: () => "react-select__input",
+          option: (state) =>
+            state.isFocused
+              ? "react-select__option react-select__option--is-focused"
+              : state.isSelected
+              ? "react-select__option react-select__option--is-selected"
+              : "react-select__option",
+        }}
+      />
 
       <button type="submit">Get Weather</button>
     </form>
